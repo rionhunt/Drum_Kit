4923039c71e624978fbc4da05637e1891a1f70af
William Hunt <rionhunt2@gmail.com>
2021-07-20 12:48:12 -0400

    I added event listeners to the keyboard and attached sound objects to the appropriate keys using the switch statement so I can play drums with the keyboard.

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
  Purpose   : This is the JS file to accompany the short project, Drum Kit. The Purpose
              of the project is to attach background images to buttons, to attach sound  
              objects to the buttons using the switch statement and to attach event listeners
-             to the buttons.
+             to the buttons. We also attached evenlisteners to the keyboard and used the 
+             switch statement to attach sound objects to the relevant keys to play drum 
+             sounds. Unassigned keys print to the console.
 */
-
+"use strict";
 var numOfBtns = document.querySelectorAll(".drum").length;
 var i = 0;
 // Add event listeners to all buttons
@@ -57,8 +59,51 @@ for (i = 0; i < numOfBtns; i++) {
   });
 }
 
-/*
-document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-  var audio = new Audio("sounds/tom-1.mp3");
-  audio.play();
-*/
+// add keydown event listener
+
+document.addEventListener("keydown", (event) => {
+  makeSound(event.key);
+});
+
+// Use event.key with switch to choose a drum sound
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    default:
+      console.log(key);
+  }
+}
