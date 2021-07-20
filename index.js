9bd232fff170e0d25537fd58f722ad76369087cd
William Hunt <rionhunt2@gmail.com>
2021-07-20 16:11:04 -0400

    Consolidated some code into a function. Added animation to flash the button when activated.

--- index.js
+++ index.js
@@ -7,62 +7,25 @@
              objects to the buttons using the switch statement and to attach event listeners
              to the buttons. We also attached evenlisteners to the keyboard and used the 
              switch statement to attach sound objects to the relevant keys to play drum 
-             sounds. Unassigned keys print to the console.
+             sounds. Unassigned keys print to the console. Next I added some animation to 
+             flash the keys when activated by the mouse or keyboard.
 */
-"use strict";
+
 var numOfBtns = document.querySelectorAll(".drum").length;
 var i = 0;
 // Add event listeners to all buttons
 for (i = 0; i < numOfBtns; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
-
-    // Determine which button is clicked, and play the sound object attached to it.
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "k":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      case "l":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-
-      default:
-        console.log(buttonInnerHTML);
-    }
+    console.log(buttonInnerHTML);
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
-// add keydown event listener
-
 document.addEventListener("keydown", (event) => {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 // Use event.key with switch to choose a drum sound
@@ -106,4 +69,13 @@ function makeSound(key) {
     default:
       console.log(key);
   }
+}
+
+// flash the button being clicked keypressed
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 50);
 }
